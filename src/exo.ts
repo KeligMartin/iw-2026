966b8f7804364fe5b907f29771e119f7e04d81fc
kmartin <kmartin@feelagile.com>
2026-03-10 14:35:05 +0100

    typescript

---
+++ src/exo.ts
@@ -0,0 +1,30 @@
+class Utilisateur {
+
+    private name: string;
+    age: number;
+    solde: number;
+
+    constructor(name: string, age: number, solde: number) {
+        this.name = name;
+        this.age = age;
+        this.solde = solde;
+    }
+
+    get userName() {
+        return this.name;
+    }
+
+    passerCommande(): void {
+        // A FAIRE
+    }
+
+    afficherCommande(): void {
+        // A FAIRE
+    }
+}
+
+interface Repas {
+    prix: number;
+    description: string;
+}
+
